7a66f8aecded841ba28ef1c855f1d9e2d247c0ea
Claudia Cortes <claudiacortes@Claudias-MacBook-Pro.local>
2026-06-04 18:37:14 -0600

    clase 4 de junio: laberinto parte final

--- Semana 7/clase_02062025_laberinto/src/clase_02062025_laberinto/Clase_02062025_laberinto.java
+++ Semana 7/clase_02062025_laberinto/src/clase_02062025_laberinto/Clase_02062025_laberinto.java
@@ -5,6 +5,7 @@
 package clase_02062025_laberinto;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -29,18 +30,113 @@ public class Clase_02062025_laberinto {
         => los obstaculos,  el tesoro y el jugador  se generan dinamicamente
          */
 
-        char[][] tablero = new char[3][3];
-        int obstaculos = 2;
-        tablero = llenartablero(tablero,obstaculos);
-        imprimirTablero(tablero);
+        char[][] tablero = new char[7][8];
+        int obstaculos = 4;
+        tablero = llenartablero(tablero, obstaculos);
+      while (true) {
+             imprimirTablero(tablero);
+             tablero = moverJugador(tablero);
+        }
     }
-    
-    public static void imprimirTablero(char[][] tablero){
-         for (int i = 0; i < tablero.length; i++) { // filas 
+
+    public static int[] buscarJugador(char[][] tablero) {
+        int fila = -1;
+        int columna = -1;
+        int[] pos = new int[2];
+        for (int i = 0; i < tablero.length; i++) {// filas 
+            for (int j = 0; j < tablero[i].length; j++) { //columnas
+                if (tablero[i][j] == 'P') {
+                    //encontramos al jugador.
+                    fila = i;
+                    columna = j;
+                }
+            }
+        }//fin del for 
+        pos[0] = fila;
+        pos[1] = columna;
+        return pos;
+    }
+
+    public static char[][] moverJugador(char[][] tablero) {
+        int[] pos = buscarJugador(tablero);
+        int filajugador = pos[0];
+        int columnaJugador = pos[1];
+        Scanner entrada = new Scanner(System.in);
+        int direccion = 0;
+        do {
+            System.out.println("1. arriba");
+            System.out.println("2. abajo");
+            System.out.println("3. Izq");
+            System.out.println("4. der");
+            direccion = entrada.nextInt();
+        } while (direccion < 1 || direccion > 4);
+
+        int tamanoFilas = tablero.length;
+        int tamanoColumnas = tablero[0].length;
+
+        switch (direccion) {
+            case 1 -> { // arriba , restar 1 a la fila 
+                if (filajugador - 1 < 0) {
+                    System.out.println("No puede salir del tablero");
+                } else {
+                    //nos podemos mover 
+                    if (tablero[filajugador - 1][columnaJugador] == 'X') {
+                        System.out.println("Obstaculo! no te puedes mover");
+                    } else {
+                        tablero[filajugador ][columnaJugador] =' ';
+                        tablero[filajugador - 1][columnaJugador] = 'P';
+                    }
+                }
+            }
+            case 2 -> { // abajo, sumar 1 a la fila 
+                if (filajugador + 1 >= tamanoFilas) {
+                    System.out.println("No puede salir del tablero");
+                } else {
+                    //nos podemos mover 
+                    if (tablero[filajugador + 1][columnaJugador] == 'X') {
+                        System.out.println("Obstaculo! no te puedes mover");
+                    } else {
+                        tablero[filajugador ][columnaJugador] =' ';
+                        tablero[filajugador + 1][columnaJugador] = 'P';
+                    }
+                }
+            }
+            case 3 -> { //izq , restar 1 a la columna 
+                if (columnaJugador - 1 < 0) {
+                    System.out.println("No puede salir del tablero");
+                } else {
+                    //nos podemos mover 
+                    if (tablero[filajugador][columnaJugador - 1] == 'X') {
+                        System.out.println("Obstaculo! no te puedes mover");
+                    } else {
+                        tablero[filajugador ][columnaJugador] =' ';
+                        tablero[filajugador][columnaJugador - 1] = 'P';
+                    }
+                }
+            }
+            case 4 -> {// derecha , sumar 1 a la columna 
+                if (columnaJugador + 1 >= tamanoColumnas) {
+                    System.out.println("No puede salir del tablero");
+                } else {
+                    //nos podemos mover 
+                    if (tablero[filajugador][columnaJugador + 1] == 'X') {
+                        System.out.println("Obstaculo! no te puedes mover");
+                    } else {
+                        tablero[filajugador ][columnaJugador] =' ';
+                        tablero[filajugador][columnaJugador + 1] = 'P';
+                    }
+                }
+            }
+        }// fin del switch 
+        return tablero;
+    }
+
+    public static void imprimirTablero(char[][] tablero) {
+        for (int i = 0; i < tablero.length; i++) { // filas 
             for (int j = 0; j < tablero[i].length; j++) {// columnas
-                System.out.print("["+tablero[i][j]+"]");
+                System.out.print("[" + tablero[i][j] + "]");
             }// fin del for 
-             System.out.println("");
+            System.out.println("");
         }// fin del for 
     }
 
@@ -51,37 +147,36 @@ public class Clase_02062025_laberinto {
             }// fin del for 
         }// fin del for 
         // obstaculos, jugador, tesoro 
-       
+
         tablero[0][0] = 'S';
-        tablero = agregarElementoRandom(tablero,'T');
-        tablero = agregarElementoRandom(tablero,'P');
+        tablero = agregarElementoRandom(tablero, 'T');
+        tablero = agregarElementoRandom(tablero, 'P');
         int contador = 0;
         while (contador < obstaculos) {
-           tablero = agregarElementoRandom(tablero,'X');
+            tablero = agregarElementoRandom(tablero, 'X');
             contador++;
         }
         return tablero;
     }// fin del metodo 
-    
-    
-    public static char[][] agregarElementoRandom(char [][] tablero, char elemento){
+
+    public static char[][] agregarElementoRandom(char[][] tablero, char elemento) {
         // calcular las dimensiones del tablero
-         int tamanoFilas = tablero.length;
+        int tamanoFilas = tablero.length;
         int tamanoColumnas = tablero[0].length;
-        
+
         Random r = new Random();
         // calcular pos random en base a l rango de pos validad 
         int fila = r.nextInt(0, tamanoFilas); //0 
         int columna = r.nextInt(0, tamanoColumnas); // 0 
-        
+
         //Revisar que la pos generada no elementos  
-        while (tablero[fila][columna] != ' ') { 
+        while (tablero[fila][columna] != ' ') {
             fila = r.nextInt(1, tamanoFilas);
             columna = r.nextInt(1, tamanoColumnas);
         }
         //agregar al tablero el elemento en una pos random 
         tablero[fila][columna] = elemento;
-        
+
         return tablero;
     }// fin del metodo
 
